577d109d4378320ba457795522ec660f4d5d3705
FunkyZc <zhoucan38@126.com>
2022-06-16 14:33:43 +0800

    整理代码

--- src/android/LocationPlugin.java
+++ src/android/LocationPlugin.java
@@ -31,21 +31,26 @@ public class LocationPlugin extends CordovaPlugin {
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 		if (locationClient == null) {
 			LocationClientOption option = new LocationClientOption();
-      option.setCoorType( "bd09ll" ); // 可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
-      option.setScanSpan(3000); // 可选，默认0，即仅定位一次，设置发起连续定位请求的间隔需要大于等于1000ms才是有效的
-      option.setIsNeedAddress(true); // 可选，设置是否需要地址信息，默认不需要
-      option.setIsNeedLocationDescribe(true); // 可选，设置是否需要地址描述
-      option.setNeedDeviceDirect(false); // 可选，设置是否需要设备方向结果
-      option.setLocationNotify(false); // 可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
-      option.setIgnoreKillProcess(true); // 可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop
-      option.setIsNeedLocationDescribe(true); // 可选，默认false，设置是否需要位置语义化结果，可以在BDLocation
-      option.setIsNeedLocationPoiList(true); // 可选，默认false，设置是否需要POI结果，可以在BDLocation
-      option.SetIgnoreCacheException(false); // 可选，默认false，设置是否收集CRASH信息，默认收集
-      option.setLocationMode(LocationMode.Hight_Accuracy); // 可选，默认高精度，设置定位模式，高精度，低功耗，仅设备，模糊
-      option.setIsNeedAltitude(false); // 可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
-      // 可选，设置首次定位时选择定位速度优先还是定位准确性优先，默认为速度优先
-      option.setFirstLocType(LocationClientOption.FirstLocType.SPEED_IN_FIRST_LOC);
-			locationClient = new LocationClient(cordova.getActivity().getApplicationContext(), option);
+			option.setCoorType( "bd09ll" ); // 可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
+			option.setScanSpan(3000); // 可选，默认0，即仅定位一次，设置发起连续定位请求的间隔需要大于等于1000ms才是有效的
+			option.setIsNeedAddress(true); // 可选，设置是否需要地址信息，默认不需要
+			option.setIsNeedLocationDescribe(true); // 可选，设置是否需要地址描述
+			option.setNeedDeviceDirect(false); // 可选，设置是否需要设备方向结果
+			option.setLocationNotify(false); // 可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+			option.setIgnoreKillProcess(true); // 可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop
+			option.setIsNeedLocationDescribe(true); // 可选，默认false，设置是否需要位置语义化结果，可以在BDLocation
+			option.setIsNeedLocationPoiList(true); // 可选，默认false，设置是否需要POI结果，可以在BDLocation
+			option.SetIgnoreCacheException(false); // 可选，默认false，设置是否收集CRASH信息，默认收集
+			option.setLocationMode(LocationMode.Hight_Accuracy); // 可选，默认高精度，设置定位模式，高精度，低功耗，仅设备，模糊
+			option.setIsNeedAltitude(false); // 可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
+			// 可选，设置首次定位时选择定位速度优先还是定位准确性优先，默认为速度优先
+			option.setFirstLocType(LocationClientOption.FirstLocType.SPEED_IN_FIRST_LOC);
+			try {
+				locationClient = new LocationClient(cordova.getActivity().getApplicationContext());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			locationClient.setLocOption(option);
 			locationClient.registerLocationListener(locationListener);
 		}
 		super.initialize(cordova, webView);
@@ -69,14 +74,14 @@ public class LocationPlugin extends CordovaPlugin {
 		if (ACTION_GETLOCATION.equals(action.toLowerCase(Locale.CHINA))) {
 			locationClient.start();
 			PluginResult r = new PluginResult(PluginResult.Status.NO_RESULT);
-      r.setKeepCallback(true);
-      callbackContext.sendPluginResult(r);
+			r.setKeepCallback(true);
+			callbackContext.sendPluginResult(r);
 			return true;
 		}
 		return false;
 	}
 
-	private class LocationListener implements BDLocationListener {
+	private class LocationListener extends BDAbstractLocationListener {
 		@Override
 		public void onReceiveLocation(BDLocation loc) {
 			if (locationClient.isStarted()) {
--- www/location.js
+++ www/location.js
@@ -2,7 +2,7 @@
 var cordova = require('cordova');
 
 const baiduLocation = {
-  getDistance: function (success, error) {
+  getlocation: function (success, error) {
       exec(success, error, 'LocationPlugin', 'getlocation', []);
   }
 }
